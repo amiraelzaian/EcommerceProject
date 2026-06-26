913b6841a714aaaffe82f39dea98ca6da611c870
Amira Alzaian <amiraelzaian204@gmail.com>
2026-06-27 00:05:20 +0300

    Add express-validator for category validation and implement validation middleware

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { param, validationResult } = require("express-vlidator");
 const {
   createCategory,
   getCategories,
@@ -6,13 +7,19 @@ const {
   updateCategory,
   deleteCategory,
 } = require("../controllers/category.controller");
+const validatorMiddleware = require("../middlewares/validatorMiddleware");
+const {
+  getCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} = require("../utils/validators/categoryValidator");
 const router = express.Router();
 
 router.route("/").post(createCategory).get(getCategories);
 router
   .route("/:id")
-  .get(getCategory)
-  .patch(updateCategory)
-  .delete(deleteCategory);
+  .get(getCategoryValidator, getCategory)
+  .patch(updateCategoryValidator, updateCategory)
+  .delete(deleteCategoryValidator, deleteCategory);
 
 module.exports = router;
