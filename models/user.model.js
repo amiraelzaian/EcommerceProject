4c203987156543c906aebc7a7a311882d7b21d7f
Amira Alzaian <amiraelzaian204@gmail.com>
2026-07-05 01:38:44 +0300

    Refactor code structure for improved readability and maintainability

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -36,5 +37,14 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+// in mongoose 9+, don't use next , use async/await
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
 const User = mongoose.model("Model", userSchema);
 module.exports = User;
