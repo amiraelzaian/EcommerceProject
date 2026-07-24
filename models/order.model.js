aa3128faf4732297b84944b1ccd686a4b4644a04
Amira Alzaian <amiraelzaian204@gmail.com>
2026-07-24 23:13:06 +0300

    feat: add order model with schema for managing orders

---
+++ models/order.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Order must be belong to user"],
+    },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        color: String,
+        price: Number,
+      },
+    ],
+    taxPrice: {
+      type: Number,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0,
+    },
+    totalOrderPirce: { type: Number },
+    paymentMethod: {
+      type: String,
+      enum: ["cash", "card"],
+      default: "cash",
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: Date,
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Order", orderSchema);
